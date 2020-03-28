dca34dc1f710abe6f77b0836d094781eb6c22095
Kulbir Tivana <kulbirtivana@HarmansiphoneXI.hitronhub.home>
2020-03-28 10:54:53 -0600

    my todo app

---
+++ js/dom.js
@@ -0,0 +1,6 @@
+//SELECT ELEMENTS AND ASSIGN THEM TO VARS
+var newTask = document.querySelector('#new-task');
+var addTaskBtn = document.querySelector('#addTask');
+
+var toDoUl = document.querySelector(".todo-list ul");
+var completeUl =  document.querySelector(".complete-list ul");
